a9a063759abc9f6772439c36865fb3b9f8ff8a23
nourhen slimen <slimennorhen1995@gmail.com>
2020-11-30 10:41:56 +0100

    my first commit

--- src/Component/Description.js
+++ src/Component/Description.js
@@ -4,13 +4,12 @@ import {Link} from 'react-router-dom'
 
 function Description(props) {
     const description = props.location.state
-    const trailer= props.location.state
+
     return (
         <div>
            <h1>Description</h1>
     <h3>{description}</h3> 
-    <h>Trailer</h>
-    <p>{trailer}</p>
+
 
     <Link to='/'>back home</Link>
         </div>
